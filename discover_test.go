5318d2499b601da58bbeb256b8b9460c3317877c
Harald Albrecht <hidden@hidden.org>
2020-01-28 20:30:20 +0100

    adds test case for hidden intermediate user namespace

--- discover_test.go
+++ discover_test.go
@@ -16,13 +16,105 @@ package lxkns
 
 import (
 	"encoding/json"
+	"io"
 	"os/exec"
+	"syscall"
+	"time"
 
 	t "github.com/thediveo/lxkns/nstypes"
+	r "github.com/thediveo/lxkns/relations"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
+
+var _ = Describe("Discover", func() {
+
+	It("finds the namespaces lsns finds", func() {
+		allns := Discover(FullDiscovery)
+		for _, ns := range lsns() {
+			nsidx := TypeIndex(t.NameToType(ns.Type))
+			dns := allns.Namespaces[nsidx][ns.NS]
+			Expect(dns).NotTo(BeZero())
+			Expect(dns.LeaderPIDs()).To(ContainElement(PIDType(ns.PID)))
+		}
+	})
+
+	It("finds hidden hierarchical user namespaces", func() {
+		cmd := NewCmd(
+			"unshare", "-Ur", "unshare", "-U",
+			"bash", "-c",
+			`readlink /proc/self/ns/user | sed -n -e 's/^.\+:\[\(.*\)\]/\1/p' && read`)
+		defer cmd.Close()
+		var usernsid t.NamespaceID
+		cmd.Decode(&usernsid)
+		allns := Discover(FullDiscovery)
+		userns := allns.Namespaces[UserNS][usernsid].(Hierarchy)
+		Expect(userns).NotTo(BeNil())
+		ppusernsid, _ := r.ID("/proc/self/ns/user")
+		Expect(userns.Parent().Parent().(Namespace).ID()).To(Equal(ppusernsid))
+	})
+
+})
+
+type Cmd struct {
+	cmd             *exec.Cmd
+	stdinr, stdoutr *io.PipeReader
+	stdinw, stdoutw *io.PipeWriter
+	dec             *json.Decoder
+}
+
+func NewCmd(command string, args ...string) *Cmd {
+	cmd := &Cmd{
+		cmd: exec.Command(command, args...),
+	}
+	cmd.cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
+	inReader, inWriter := io.Pipe()
+	cmd.cmd.Stdin = inReader
+	cmd.stdinr = inReader
+	cmd.stdinw = inWriter
+	outReader, outWriter := io.Pipe()
+	cmd.cmd.Stdout = outWriter
+	cmd.stdoutr = outReader
+	cmd.stdoutw = outWriter
+	cmd.dec = json.NewDecoder(cmd.stdoutr)
+	cmd.cmd.Start()
+	return cmd
+}
+
+// Close completes the command by sending it an ENTER input and then closing
+// the input pipe to the command. Then close waits at most 2s for the command
+// to finish its business. If the command passes the timeout, then it will be
+// killed hard.
+func (cmd *Cmd) Close() {
+	cmd.Proceed()
+	cmd.stdinw.Close()
+	done := make(chan error)
+	go func() { done <- cmd.cmd.Wait() }()
+	select {
+	case <-time.After(2 * time.Second):
+		// And if thou'rt unwilling...
+		cmd.cmd.Process.Kill()
+	case <-done:
+	}
+	cmd.stdinr.Close()
+	cmd.stdoutr.Close()
+	cmd.stdoutw.Close()
+}
+
+// Proceed sends the command an ENTER input.
+func (cmd *Cmd) Proceed() {
+	cmd.stdinw.Write([]byte{0x0a})
+}
+
+// Decode reads JSON from the command's output and tries to decode it into the
+// data element specified.
+func (cmd *Cmd) Decode(v interface{}) {
+	err := cmd.dec.Decode(v)
+	if err != nil {
+		panic(err)
+	}
+}
 
 type lsnsentry struct {
 	NS      t.NamespaceID `json:"ns"`
@@ -41,24 +133,16 @@ func lsns(opts ...string) []lsnsentry {
 	out, err := exec.Command(
 		"lsns",
 		append([]string{"--json"}, opts...)...).Output()
-	Expect(err).NotTo(HaveOccurred())
+	if err != nil {
+		panic(err)
+	}
 	var res lsnsdata
 	err = json.Unmarshal(out, &res)
-	Expect(err).NotTo(HaveOccurred())
-	Expect(res.Namespaces).NotTo(BeEmpty())
+	if err != nil {
+		panic(err)
+	}
+	if len(res.Namespaces) == 0 {
+		panic("error: no namespaces read")
+	}
 	return res.Namespaces
 }
-
-var _ = Describe("Discover", func() {
-
-	It("sees the namespaces lsns sees", func() {
-		allns := Discover(FullDiscovery)
-		for _, ns := range lsns() {
-			nsidx := TypeIndex(t.NameToType(ns.Type))
-			dns := allns.Namespaces[nsidx][ns.NS]
-			Expect(dns).NotTo(BeZero())
-			Expect(dns.LeaderPIDs()).To(ContainElement(PIDType(ns.PID)))
-		}
-	})
-
-})
