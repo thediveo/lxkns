705d42330c6bb2a3dd65c719ff2c6aa07e6783ae
Harald Albrecht <hidden@hidden.org>
2020-02-17 20:43:12 +0100

    adds PID translation between PID namespaces

---
+++ pidmap.go
@@ -0,0 +1,127 @@
+// Copyright 2020 Harald Albrecht.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not
+// use this file except in compliance with the License. You may obtain a copy
+// of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations
+// under the License.
+
+// +build linux
+
+package lxkns
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// NamespacedPID is PID in the context of a specific PID namespace.
+type NamespacedPID struct {
+	PIDNS Namespace // PID namespace ID for PID.
+	PID   PIDType   // PID within PID namespace (of ID).
+}
+
+// NamespacedPIDs is a list of PIDs for the same process, but in different PID
+// namespaces.
+type NamespacedPIDs []NamespacedPID
+
+// PIDMap maps a single namespaced PID to the list of PIDs for this process in
+// different PID namespaces. Further PIDMap methods then allow simple
+// translation of PIDs between different PID namespaces.
+type PIDMap struct {
+	m map[NamespacedPID]NamespacedPIDs
+}
+
+// Translate translates a PID "pid" in PID namespace "from" to the
+// corresponding PID in PID namespace "to". Returns 0, if PID "pid" either
+// does not exist in namespace "from", or PID namespace "to" isn't either a
+// parent or child of PID namespace "from".
+func (pm *PIDMap) Translate(pid PIDType, from Namespace, to Namespace) PIDType {
+	if namespacedpids, ok := pm.m[NamespacedPID{PID: pid, PIDNS: from}]; ok {
+		for _, namespacedpid := range namespacedpids {
+			if namespacedpid.PIDNS == to {
+				return namespacedpid.PID
+			}
+		}
+	}
+	return 0
+}
+
+// NewPIDMap returns a new PID map based on the specified discovery results
+// and further information gathered from the /proc filesystem.
+func NewPIDMap(res *DiscoveryResult) *PIDMap {
+	pm := &PIDMap{
+		m: map[NamespacedPID]NamespacedPIDs{},
+	}
+	for _, proc := range res.Processes {
+		// For each process, first get its list of namespaced PIDs, which
+		// lists the PIDs starting from the PID namespace we're currently in
+		// and continues into nested child PID namespaces.
+		pids := NSpid(proc)
+		pidns := proc.Namespaces[PIDNS].(Hierarchy)
+		// The namespaced PIDs are top-down, while we have to go bottom-up
+		// from the process' current PID namespace, in order to assemble the
+		// list of NamespacedPIDs correctly.
+		pidslen := len(pids)
+		if pidslen == 0 {
+			continue
+		}
+		namespacedpids := make(NamespacedPIDs, pidslen)
+		idx := 0
+		for pidns != nil {
+			namespacedpids[idx] = NamespacedPID{
+				PIDNS: pidns.(Namespace),
+				PID:   pids[pidslen-idx-1],
+			}
+			pidns = pidns.Parent()
+			idx++
+		}
+		if idx != pidslen {
+			// Did someone forgot to also discover the hierarchy???
+			continue
+		}
+		// Now index these NamespacedPIDs by the list NamespacedPID elements,
+		// so we can later quickly look up the list of namespaced PIDs for
+		// this process using (PID, PID-namespace).
+		for _, namespacedpid := range namespacedpids {
+			pm.m[namespacedpid] = namespacedpids
+		}
+	}
+	return pm
+}
+
+// NSpid returns the list of namespaced PIDs for the process proc, based on
+// information from the /proc filesystem.
+func NSpid(proc *Process) (pids []PIDType) {
+	f, err := os.Open(fmt.Sprintf("/proc/%d/status", proc.PID))
+	if err != nil {
+		return
+	}
+	defer f.Close()
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.HasPrefix(line, "NSpid:\t") {
+			pidstxts := strings.Split(line[7:], "\t")
+			pids = make([]PIDType, len(pidstxts))
+			for idx, pidtxt := range pidstxts {
+				pid, err := strconv.Atoi(pidtxt)
+				if err != nil {
+					return []PIDType{}
+				}
+				pids[idx] = PIDType(pid)
+			}
+			return
+		}
+	}
+	panic("/proc filesystem broken: no NSpid element in status.")
+}
---
+++ pidmap_test.go
@@ -0,0 +1,64 @@
+// Copyright 2020 Harald Albrecht.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not
+// use this file except in compliance with the License. You may obtain a copy
+// of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations
+// under the License.
+
+package lxkns
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/thediveo/lxkns/nstest"
+	t "github.com/thediveo/lxkns/nstypes"
+	"github.com/thediveo/testbasher"
+)
+
+var _ = Describe("maps PIDs", func() {
+
+	It("translates PIDs", func() {
+		scripts := testbasher.Basher{}
+		defer scripts.Done()
+		scripts.Common(nstest.NamespaceUtilsScript)
+		scripts.Script("main", `
+unshare -Umr $stage2
+`)
+		scripts.Script("stage2", `
+unshare -pf $pidxlas3
+`)
+		scripts.Script("pidxlas3", `
+mount -t proc proc /proc
+process_namespaceid pid # print ID of new PID namespace.
+echo "$$"
+read # wait for test to proceed()
+`)
+		cmd := scripts.Start("main")
+		defer cmd.Close()
+		var pidnsid t.NamespaceID
+		cmd.Decode(&pidnsid)
+		var leafpid PIDType
+		cmd.Decode(&leafpid)
+
+		opts := NoDiscovery
+		opts.SkipProcs = false
+		opts.SkipHierarchy = false
+		allns := Discover(opts)
+		pidns := allns.Namespaces[PIDNS][pidnsid]
+		initialpidns := allns.PIDNSRoots[0]
+
+		pidmap := NewPIDMap(allns)
+
+		pid := pidmap.Translate(leafpid, pidns, initialpidns)
+		Expect(pid).NotTo(BeZero())
+		Expect(allns.Processes[pid].Name).To(Equal("pidxlas3.sh"))
+	})
+
+})
