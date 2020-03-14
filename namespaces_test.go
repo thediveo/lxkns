a543fb5b8341f695603f1b6da24cd270c2554642
thediveo <hidden@hidden.org>
2020-03-15 00:02:53 +0100

    improves test coverage

--- namespaces_test.go
+++ namespaces_test.go
@@ -15,6 +15,10 @@
 package lxkns
 
 import (
+	"fmt"
+	"os"
+	"os/user"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"github.com/thediveo/lxkns/nstypes"
@@ -29,12 +33,13 @@ var _ = Describe("namespaces", func() {
 
 	Describe("plain namespaces", func() {
 
-		It("describe themselves", func() {
+		It("render details", func() {
 			pns := &plainNamespace{
 				nsid:   123,
 				nstype: nstypes.CLONE_NEWNET,
 			}
 			Expect(pns.TypeIDString()).To(Equal("net:[123]"))
+			Expect(pns.LeaderString()).To(Equal(""))
 
 			pns.leaders = []*Process{
 				&Process{PID: 666, Starttime: 666, Name: "foo"},
@@ -60,7 +65,7 @@ var _ = Describe("namespaces", func() {
 			Expect(s).To(ContainSubstring(`user:[777]`))
 		})
 
-		It("finds the ealdorman", func() {
+		It("find an ealdorman", func() {
 			pns := &plainNamespace{
 				leaders: []*Process{
 					&Process{PID: 666, Starttime: 666},
@@ -70,11 +75,78 @@ var _ = Describe("namespaces", func() {
 			Expect(pns.Ealdorman()).To(Equal(&Process{PID: 42, Starttime: 42}))
 		})
 
-		It("finds no ealdorman when there isn't one", func() {
+		It("find no ealdorman when there isn't one", func() {
 			pns := &plainNamespace{
 				leaders: []*Process{},
 			}
 			Expect(pns.Ealdorman()).To(BeNil())
+		})
+
+		It("lives with errors when detecting the owner", func() {
+			pns := &plainNamespace{}
+			Expect(func() { pns.DetectOwner(nil) }).NotTo(Panic())
+		})
+
+	})
+
+	Describe("hierarchical namespaces", func() {
+
+		It("render details", func() {
+			hns := &hierarchicalNamespace{
+				plainNamespace: plainNamespace{
+					nsid:   123,
+					nstype: nstypes.CLONE_NEWPID,
+				},
+			}
+			s := hns.String()
+			Expect(s).To(ContainSubstring("pid:[123]"))
+			Expect(s).To(ContainSubstring("parent none"))
+			Expect(s).To(ContainSubstring("children none"))
+
+			chns := &hierarchicalNamespace{
+				plainNamespace: plainNamespace{
+					nsid:   678,
+					nstype: nstypes.CLONE_NEWPID,
+				},
+			}
+			hns.AddChild(chns)
+			Expect(hns.String()).To(ContainSubstring("children [pid:[678]]"))
+			Expect(chns.String()).To(ContainSubstring("parent pid:[123]"))
+		})
+
+		It("cant add a child namespace twice", func() {
+			hns := &hierarchicalNamespace{}
+			chns := &hierarchicalNamespace{}
+			Expect(func() { hns.AddChild(chns) }).NotTo(Panic())
+			Expect(func() { hns.AddChild(chns) }).To(Panic())
+		})
+
+	})
+
+	Describe("user namespaces", func() {
+
+		It("render details", func() {
+			uns := NewNamespace(nstypes.CLONE_NEWUSER, nstypes.NamespaceID(1111), "").(*userNamespace)
+			uns.owneruid = os.Getuid()
+			uns.AddLeader(&Process{
+				PID:  88888,
+				Name: "foobar",
+			})
+			uns.ownedns[NetNS][1234] = &plainNamespace{
+				nsid:   1234,
+				nstype: nstypes.CLONE_NEWNET,
+			}
+
+			Expect(uns.UID()).To(Equal(uns.owneruid))
+			Expect(uns.Owned()[NetNS]).To(HaveLen(1))
+
+			s := uns.String()
+			Expect(s).To(ContainSubstring(`joined by "foobar" (88888)`))
+			Expect(s).To(ContainSubstring("owning [net:[1234]]"))
+			Expect(s).To(ContainSubstring("user:[1111]"))
+			u, _ := user.Current()
+			Expect(s).To(ContainSubstring(
+				fmt.Sprintf(`UID %d ("%s")`, os.Geteuid(), u.Username)))
 		})
 
 	})
