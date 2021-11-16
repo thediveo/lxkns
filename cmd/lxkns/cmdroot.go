595bf7c5ca964bb6eb14d61e2dfea551cb56c364
Harald Albrecht <hidden@hidden.org>
2021-11-16 20:05:32 +0100

    refactoring, fixing gocyclo findings

--- cmd/lxkns/cmdroot.go
+++ cmd/lxkns/cmdroot.go
@@ -38,6 +38,100 @@ import (
 	"golang.org/x/sys/unix"
 )
 
+// reexec reexecutes itself in the initial cgroup namespace, if necessary, and
+// only then terminates this process. In case of re-execution errors, it logs
+// these errors and then exits, too. If switching wasn't necessary, it simply
+// returns.
+func reexec() {
+	// Let's check if we're in a non-initial cgroup, such as when running
+	// inside a Docker container on a "pure" unified cgroups v2 hierarchy
+	// and with container cgroup'ing enabled...
+	initialcgroupns := ops.NewTypedNamespacePath("/proc/1/ns/cgroup", species.CLONE_NEWCGROUP)
+	initialcgroupnsid, ierr := initialcgroupns.ID()
+	currentcgroupnsid, cerr := ops.NewTypedNamespacePath("/proc/self/ns/cgroup", species.CLONE_NEWCGROUP).ID()
+	if ierr != nil || cerr != nil {
+		log.Errorf("cannot determine initial and own cgroup namespaces, not switching cgroup namespace")
+		return
+	}
+	log.Debugf("current cgroup namespace %d, initial cgroup namespace: %d",
+		currentcgroupnsid.Ino, initialcgroupnsid.Ino)
+	if currentcgroupnsid != initialcgroupnsid {
+		// In order to safely switch the cgroup namespace in a Golang app
+		// with potentially several OS threads bouncing around by now we can
+		// only lock our current OS thread, switch into the initial cgroup
+		// namespace, and finally reexecute ourselves again. We don't use
+		// our re-execution support package here, as that on purpose is
+		// designed to not be re-executable from a child and provides a
+		// JSON-oriented result interface (which we don't need). Instead, we
+		// just use the few and simple primitives, namely
+		// runtime.LockOSThread() and ops.Execute(). Everything else is
+		// debug logging and error handling.
+		log.Infof("switching from current cgroup:[%d] into initial cgroup:[%d] and re-executing...",
+			currentcgroupnsid.Ino, initialcgroupnsid.Ino)
+		runtime.LockOSThread()
+		if res, err := ops.Execute(func() interface{} {
+			// tee hee, while the process might still be in its original,
+			// but not initial, cgroup namespace, this particular OS-level
+			// task/thread should now be in the initial cgroup namespace. So
+			// we need to query the current cgroup namespace by TID, not
+			// PID. Please note that we don't need to use
+			// "/proc/self/task/$TID/ns/cgroup", as all tasks are directly
+			// accessible at the /proc/$TID level; they're just not listed,
+			// yet still there.
+			currentcgroupnsid, _ := ops.NewTypedNamespacePath(
+				fmt.Sprintf("/proc/%d/ns/cgroup", syscall.Gettid()),
+				species.CLONE_NEWCGROUP).ID()
+			success := ""
+			if currentcgroupnsid == initialcgroupnsid {
+				success = "successfully "
+			}
+			log.Debugf("(parent) current OS thread %sswitched to cgroup:[%d]", success, currentcgroupnsid.Ino)
+			return unix.Exec(
+				"/proc/self/exe",
+				append([]string{os.Args[0], "--cgroupswitched"}, os.Args[1:]...),
+				os.Environ(),
+			)
+		}, initialcgroupns); err != nil {
+			log.Errorf("failed to switch to initial cgroup, err: %s", err.Error())
+		} else {
+			log.Errorf("failed to re-execute, err: %s", res)
+			os.Exit(1)
+		}
+	}
+}
+
+// reexeced runs in the re-executed child process and logs information about the
+// current cgroup namespace, which hopefully will be the initial cgroup
+// namespace. It fixes the process name to show the binary's base name, as
+// opposed to "exe" (which initially set due to re-execution /proc/self/exe).
+func reexeced() {
+	initialcgroupnsid, _ := ops.NewTypedNamespacePath("/proc/1/ns/cgroup", species.CLONE_NEWCGROUP).ID()
+	currentcgroupnsid, _ := ops.NewTypedNamespacePath("/proc/self/ns/cgroup", species.CLONE_NEWCGROUP).ID()
+	isInitial := ""
+	if currentcgroupnsid == initialcgroupnsid {
+		isInitial = "initial "
+	}
+	log.Infof("re-executed in %scgroup:[%d]", isInitial, currentcgroupnsid.Ino)
+	// Unfortunately, we end up here with /proc/self/stat stating our
+	// process name as "exe", because we executed our own executable. This
+	// is not terribly useful and user/admin friendly, so we try to set our
+	// own process name from our first command line argument.
+	runtime.LockOSThread() // this still runs on the main thread...!
+	proc := model.NewProcess(model.PIDType(os.Getpid()))
+	procname := append([]byte(proc.Basename()), 0)
+	ptr := unsafe.Pointer(&procname[0]) // #nosec G103
+	// prctl(PR_SET_NAME, ...) will silently truncate any process name
+	// deemed too long, see also:
+	// https://man7.org/linux/man-pages/man2/prctl.2.html
+	if _, _, errno := syscall.RawSyscall6(
+		syscall.SYS_PRCTL, syscall.PR_SET_NAME, uintptr(ptr), 0, 0, 0, 0); errno != 0 {
+		log.Errorf("cannot fix process name: %s", syscall.Errno(errno).Error())
+	} else {
+		log.Debugf("fixed re-executed process name to %q", proc.Basename())
+	}
+	runtime.UnlockOSThread()
+}
+
 // lxknsservice is the "root command" to be run after successfully parsing the
 // CLI flags. We then here kick off the lxkns service itself.
 func lxknsservice(cmd *cobra.Command, _ []string) error {
@@ -53,84 +147,9 @@ func lxknsservice(cmd *cobra.Command, _ []string) error {
 	switchedCgroup, _ := cmd.PersistentFlags().GetBool("cgroupswitched")
 	switchCgroup, _ := cmd.PersistentFlags().GetBool("initialcgroup")
 	if !switchedCgroup && switchCgroup {
-		// Let's check if we're in a non-initial cgroup, such as when running
-		// inside a Docker container on a "pure" unified cgroups v2 hierarchy
-		// and with container cgroup'ing enabled...
-		initialcgroupns := ops.NewTypedNamespacePath("/proc/1/ns/cgroup", species.CLONE_NEWCGROUP)
-		initialcgroupnsid, ierr := initialcgroupns.ID()
-		currentcgroupnsid, cerr := ops.NewTypedNamespacePath("/proc/self/ns/cgroup", species.CLONE_NEWCGROUP).ID()
-		if ierr != nil || cerr != nil {
-			log.Errorf("cannot determine initial and own cgroup namespaces, not switching cgroup namespace")
-		} else if currentcgroupnsid != initialcgroupnsid {
-			// In order to safely switch the cgroup namespace in a Golang app
-			// with potentially several OS threads bouncing around by now we can
-			// only lock our current OS thread, switch into the initial cgroup
-			// namespace, and finally reexecute ourselves again. We don't use
-			// our re-execution support package here, as that on purpose is
-			// designed to not be re-executable from a child and provides a
-			// JSON-oriented result interface (which we don't need). Instead, we
-			// just use the few and simple primitives, namely
-			// runtime.LockOSThread() and ops.Execute(). Everything else is
-			// debug logging and error handling.
-			log.Infof("switching from current cgroup:[%d] into initial cgroup:[%d] and re-executing...",
-				currentcgroupnsid.Ino, initialcgroupnsid.Ino)
-			runtime.LockOSThread()
-			if res, err := ops.Execute(func() interface{} {
-				// tee hee, while the process might still be in its original,
-				// but not initial, cgroup namespace, this particular OS-level
-				// task/thread should now be in the initial cgroup namespace. So
-				// we need to query the current cgroup namespace by TID, not
-				// PID. Please note that we don't need to use
-				// "/proc/self/task/$TID/ns/cgroup", as all tasks are directly
-				// accessible at the /proc/$TID level; they're just not listed,
-				// yet still there.
-				currentcgroupnsid, _ := ops.NewTypedNamespacePath(
-					fmt.Sprintf("/proc/%d/ns/cgroup", syscall.Gettid()),
-					species.CLONE_NEWCGROUP).ID()
-				success := ""
-				if currentcgroupnsid == initialcgroupnsid {
-					success = "successfully "
-				}
-				log.Debugf("current OS thread %sswitched to cgroup:[%d]", success, currentcgroupnsid.Ino)
-				return unix.Exec(
-					"/proc/self/exe",
-					append([]string{os.Args[0], "--cgroupswitched"}, os.Args[1:]...),
-					os.Environ(),
-				)
-			}, initialcgroupns); err != nil {
-				log.Errorf("failed to switch to initial cgroup, err: %s", err.Error())
-			} else {
-				log.Errorf("failed to re-execute, err: %s", res)
-				os.Exit(1)
-			}
-		}
+		reexec()
 	} else if switchedCgroup {
-		// After re-execution log information about the current cgroup
-		// namespace, which hopefully will be the initial cgroup namespace...
-		initialcgroupnsid, _ := ops.NewTypedNamespacePath("/proc/1/ns/cgroup", species.CLONE_NEWCGROUP).ID()
-		currentcgroupnsid, _ := ops.NewTypedNamespacePath("/proc/self/ns/cgroup", species.CLONE_NEWCGROUP).ID()
-		isInitial := ""
-		if currentcgroupnsid == initialcgroupnsid {
-			isInitial = "initial "
-		}
-		log.Infof("re-executed in %scgroup:[%d]", isInitial, currentcgroupnsid.Ino)
-		// Unfortunately, we end up here with /proc/self/stat stating our
-		// process name as "exe", because we executed our own executable. This
-		// is not terribly useful and user/admin friendly, so we try to set our
-		// own process name from our first command line argument.
-		runtime.LockOSThread() // this still runs on the main thread...!
-		proc := model.NewProcess(model.PIDType(os.Getpid()))
-		procname := append([]byte(proc.Basename()), 0)
-		ptr := unsafe.Pointer(&procname[0]) // #nosec G103
-		// prctl(PR_SET_NAME, ...) will silently truncate any process name
-		// deemed too long, see also:
-		// https://man7.org/linux/man-pages/man2/prctl.2.html
-		if _, _, errno := syscall.RawSyscall6(syscall.SYS_PRCTL, syscall.PR_SET_NAME, uintptr(ptr), 0, 0, 0, 0); errno != 0 {
-			log.Errorf("cannot fix process name: %s", syscall.Errno(errno).Error())
-		} else {
-			log.Debugf("fixed re-executed process name to %q", proc.Basename())
-		}
-		runtime.UnlockOSThread()
+		reexeced()
 	}
 
 	// And now for the real meat.
