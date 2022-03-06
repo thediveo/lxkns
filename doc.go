8d3300c9ab8f48a100059b5df63b65e986678d2a
thediveo <thediveo@gmx.eu>
2022-03-06 20:57:52 +0000

    fixes matcher package reference in documentation

--- doc.go
+++ doc.go
@@ -4,7 +4,7 @@ Package lxkns discovers Linux kernel namespaces. Please see the discovery
 (sub)package for the specific API to namespace discovery.
 
 TDD support can be found in the test/matchers (sub)package implementing
-domain-specific Gomega (https://onsi.github.io/gomega) matchers about
+domain-specific Gomega (https://onsi.github.io/gomega) matcher about
 containers, container groups, pods, et cetera.
 
 */
