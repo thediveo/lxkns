bacd29a003f9e7575cf868d639f80902f5525a11
TheDiveO <6920158+thediveo@users.noreply.github.com>
2022-11-03 20:09:56 +0100

    tweaks getstdout test to not spill into stdout

--- cmd/internal/test/getstdout/getstdout_test.go
+++ cmd/internal/test/getstdout/getstdout_test.go
@@ -15,16 +15,39 @@
 package getstdout
 
 import (
+	"io"
 	"os"
 	"strings"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/gbytes"
 )
 
 var _ = Describe("captures stdout", func() {
 
 	It("captures really looooong output", func() {
+		By("redirecting the normal stdout and stderr into a buffer")
+		oldStdout := os.Stdout
+		oldStderr := os.Stderr
+		defer func() {
+			os.Stdout = oldStdout
+			os.Stderr = oldStderr
+		}()
+		r, w, err := os.Pipe()
+		Expect(err).NotTo(HaveOccurred())
+		defer func() {
+			_ = r.Close()
+			_ = w.Close()
+		}()
+		buff := gbytes.NewBuffer()
+		go func() {
+			_, _ = io.Copy(buff, r)
+		}()
+		os.Stdout = w
+		os.Stderr = w
+
+		By("testing that output before and after Stdouterr ends up in our buffer, and Stdouterr returns the output of its fn")
 		s := strings.Repeat("Nobody expects the spanish inquisition! ", 10000)
 		os.Stdout.WriteString("foobar!")
 		out := Stdouterr(func() { os.Stdout.WriteString(s) })
@@ -32,6 +55,7 @@ var _ = Describe("captures stdout", func() {
 		Expect(out).To(Equal(s))
 		out = Stdouterr(func() { os.Stderr.WriteString(s) })
 		Expect(out).To(Equal(s))
+		Expect(buff).To(gbytes.Say("foobar!foobar!"))
 	})
 
 })
