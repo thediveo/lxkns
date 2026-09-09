f70067c18daa77fcba0a0073a0d6df317fd3e9ea
thediveo <thediveo@gmx.eu>
2026-09-09 13:35:28 +0000

    fix: crash in visitor upon nil owning user namespace
    Signed-off-by: thediveo <thediveo@gmx.eu>

--- cmd/lspidns/visitor.go
+++ cmd/lspidns/visitor.go
@@ -61,8 +61,11 @@ func (v *PIDNSVisitor) Roots(roots any) []any {
 		// these user namespaces instead of the root PID namespaces.
 		uniqueUserNamespaces := map[species.NamespaceID]model.Namespace{}
 		for _, pidns := range roots.([]model.Namespace) {
-			userns := pidns.Owner().(model.Namespace)
-			// we don't buffer with checking if we've already seen this owning
+			userns, _ := pidns.Owner().(model.Namespace)
+			if userns == nil {
+				continue
+			}
+			// we don't bother with checking if we've already seen this owning
 			// user namespace, just put it into the map, overwriting itself in
 			// case.
 			uniqueUserNamespaces[userns.ID()] = userns
